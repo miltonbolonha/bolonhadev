0877cdf1dd320cc9df954747bac8849772b369c7
bolonhadev <fala@bolonha.dev>
2019-11-05 16:13:42 -0300

    BDB-01: Feature: Git init

---
+++ bolonhaDev/src/pages/404.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const NotFoundPage = () => (
+  <Layout>
+    <SEO title="404: Not found" />
+    <h1>Luv Green Day</h1>
+    <p>What you looking for... ?</p>
+  </Layout>
+)
+
+export default NotFoundPage
---
+++ bolonhaDev/src/pages/index.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>@BolonhaDev</h1>
+    <p>in site</p>
+    <p>on master <span>[+]</span></p>
+  </Layout>
+)
+
+export default IndexPage
